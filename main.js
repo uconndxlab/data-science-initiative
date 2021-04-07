b152f1bbdcd390b4406f7c0d10c5daad428f3fed
Emily Ha <emily.h@uconn.edu>
2021-04-07 00:10:12 -0400

    new pages

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-//image parallax
-window.addEventListener('scroll', function(e){
-    const target = document.querySelectorAll('.scroll');
-
-
-
-    var index = 0, length = target.length;
-    for(index; index <length; index++){
-        var pos = window.pageYOffset * target[index].dataset.rate;
-        
-        target[index].style.transform = 'translate3d(0px,'+pos+'px,0px)';
-        
-       
-    }
-
-})
-
-/*
-let mouseCursor = document.querySelector('.cursor');
-let innerMouseCursor = document.querySelector('.inner-cursor');
-
-window.addEventListener('mousemove', cursor);
-
-function cursor(e){
-        mouseCursor.style.top = e.pageY + 'px';
-        mouseCursor.style.left = e.pageX + 'px';
-
-        innerMouseCursor.style.top = e.pageY + 'px';
-        innerMouseCursor.style.left = e.pageX + 'px';
-
-}
-
-*/
-
 //background change on components
 window.sections = [...document.querySelectorAll('.panel')];
 window.lastScrollTop = window.pageYOffset;
@@ -52,5 +18,4 @@ function onScroll() {
     .find(section => section.rect.bottom >= (window.innerHeight * 0.5));
   document.body.style.background = section.el.getAttribute('data-bg');
 }
-
 
---
+++ where-who.js
@@ -0,0 +1,36 @@
+
+//hover for where/who page
+var country = document.querySelector('#country-hover');
+var state = document.querySelector('#state-hover');
+var city = document.querySelector('#city-hover');
+var street = document.querySelector('#street-hover');
+var building = document.querySelector('#building-hover');
+var suite = document.querySelector('#suite-hover');
+var imgInsert = document.querySelector('.img-insert');
+var whereBold = document.querySelector('.where-bold');
+var whereNormal = document.querySelector('.where-normal');
+
+country.addEventListener("click", function(){
+  imgInsert.src = "img/where-imgs/nasa-usa.png";
+  imgInsert.style.transition = ".5s ease";
+})
+
+state.addEventListener("click", function(){
+  imgInsert.src = "img/where-imgs/connecticut-img.png";
+  imgInsert.style.transition = ".5s ease";
+})
+
+city.addEventListener("click", function(){
+  imgInsert.src = "img/where-imgs/stamford-img.png";
+  imgInsert.style.transition = ".5s ease";
+})
+
+building.addEventListener("click", function(){
+  imgInsert.src = "img/where-imgs/9wbroadst.png";
+  imgInsert.style.transition = ".5s ease";
+})
+
+suite.addEventListener("click", function(){
+  imgInsert.src = "img/where-imgs/StamfordTIP1.jpg";
+  imgInsert.style.transition = ".5s ease";
+})
